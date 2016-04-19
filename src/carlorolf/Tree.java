da30042df41f534ef06f38cdc449806de373dc6a
Carl Rosén <carro311@student.liu.se>
2016-04-19 15:17:02 +0200

    grass pictures

--- src/carlorolf/Tree.java
+++ src/carlorolf/Tree.java
@@ -4,7 +4,7 @@ public class Tree extends ArenaObject
 {
 
     public Tree(final int x, final int y, CollisionHandler collisionHandler) {
-	super(x, y, 1.2, 1.2, 0.1, ShapeEnum.RECTANGLE, false, Images.getImage("tree.png"), collisionHandler);
+	super(x, y, 1.2, 1.2, 0.1, ShapeEnum.RECTANGLE, false, Images.getImage("tree_trans.png"), collisionHandler);
     }
 
     @Override public void Collision(final CollisionEvent e) {
