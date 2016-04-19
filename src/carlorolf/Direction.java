e5eb913ef76f2334ecd6125304b9e00dd2296b5d
Carl Rosén <carro311@student.liu.se>
2016-04-19 16:01:07 +0200

    Direction toString

--- src/carlorolf/Direction.java
+++ src/carlorolf/Direction.java
@@ -2,5 +2,30 @@ package carlorolf;
 
 public enum Direction
 {
-    NORTH, SOUTH, EAST, WEST, NORTHWEST, NORTHEAST, SOUTHWEST, SOUTHEAST, NONE
-}
+    NORTH, SOUTH, EAST, WEST, NORTHWEST, NORTHEAST, SOUTHWEST, SOUTHEAST, NONE;
+
+    public String getString() {
+	switch (this) {
+	    case NORTH:
+		return "north";
+	    case SOUTH:
+		return "south";
+	    case EAST:
+		return "east";
+	    case WEST:
+		return "west";
+	    case NORTHWEST:
+		return "northwest";
+	    case SOUTHWEST:
+		return "southwest";
+	    case NORTHEAST:
+		return "northeast";
+	    case SOUTHEAST:
+		return "southeast";
+	    case NONE:
+		return "";
+
+	}
+
+    }
+    }
