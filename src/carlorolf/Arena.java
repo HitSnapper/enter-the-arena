8a797c3127dd348943b56f934c6a5c85cba6cf6a
Carl Rosén <carro311@student.liu.se>
2016-04-27 16:31:42 +0200

    random ground

--- src/carlorolf/Arena.java
+++ src/carlorolf/Arena.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Arena {
+public class Arena
+{
     private int width;
     private int height;
     private List<ArenaListener> arenaListeners;
@@ -18,133 +19,143 @@ public class Arena {
     private boolean gameOver;
 
     public Arena(int width, int height, CollisionHandler collisionHandler) {
-        gameOver = false;
-        this.collisionHandler = collisionHandler;
-        this.width = width;
-        this.height = height;
-        this.arenaListeners = new ArrayList();
-        this.backgroundList = new ArrayList();
-        this.objects = new ArrayList();
-        this.layerList = new ArrayList<>();
-        this.removeObjectsList = new ArrayList<>();
-        this.topLayers = new ArrayList<>();
-        generateArena();
+	gameOver = false;
+	this.collisionHandler = collisionHandler;
+	this.width = width;
+	this.height = height;
+	this.arenaListeners = new ArrayList();
+	this.backgroundList = new ArrayList();
+	this.objects = new ArrayList();
+	this.layerList = new ArrayList<>();
+	this.removeObjectsList = new ArrayList<>();
+	this.topLayers = new ArrayList<>();
+	generateArena();
     }
 
     public int getWidth() {
-        return width;
+	return width;
     }
 
     public int getHeight() {
-        return height;
+	return height;
     }
 
     public void addArenaListener(ArenaListener listener) {
-        arenaListeners.add(listener);
+	arenaListeners.add(listener);
     }
 
     public List<VisibleObject> getTopLayers() {
-        return topLayers;
+	return topLayers;
     }
 
     public void update(double deltaTime) {
-        for (ArenaObject object : removeObjectsList) {
-            objects.remove(object);
-        }
-        for (ArenaObject arenaObject : objects) {
-            arenaObject.update(deltaTime);
-        }
+	for (ArenaObject object : removeObjectsList) {
+	    objects.remove(object);
+	}
+	for (ArenaObject arenaObject : objects) {
+	    arenaObject.update(deltaTime);
+	}
 
-        if (getPlayer().isDead()) {
-            gameOver = true;
-        }
+	if (getPlayer().isDead()) {
+	    gameOver = true;
+	}
 
-        notifyListeners();
+	notifyListeners();
     }
 
     public void addBackgroundLayer(VisibleObject object) {
-        layerList.add(object);
+	layerList.add(object);
     }
 
-    public void addTopLayer(VisibleObject object){
-        topLayers.add(object);
+    public void addTopLayer(VisibleObject object) {
+	topLayers.add(object);
     }
 
-    public void removeTopLayer(VisibleObject object){
-        topLayers.remove(object);
+    public void removeTopLayer(VisibleObject object) {
+	topLayers.remove(object);
     }
 
     private void notifyListeners() {
-        arenaListeners.forEach(ArenaListener::arenaChanged);
+	arenaListeners.forEach(ArenaListener::arenaChanged);
     }
 
     private void generateArena() {
-        generateBackground();
-        player = new Player(2.5, 2.5, collisionHandler, this);
-        objects.add(player);
+	generateBackground();
+	player = new Player(2.5, 2.5, collisionHandler, this);
+	objects.add(player);
 
 	Random rand = new Random();
 
-        for(int i = 0; i < 2; i++){
-            objects.add(new Enemy(rand.nextInt(width - 2) + 1, rand.nextInt(width - 2)+1, collisionHandler, this));
-            objects.add(new DragonBoss(rand.nextInt(width - 2) + 1, rand.nextInt(width - 2)+1, collisionHandler, this));
-            objects.add(new Stone(rand.nextInt(width - 2) + 1, rand.nextInt(width - 2)+1, 1.5, 1.5, collisionHandler, this));
-            objects.add(new MovableObject(rand.nextInt(width - 2) + 1, rand.nextInt(width - 2)+1, collisionHandler, this));
-            objects.add(new Tree(rand.nextInt(width - 2) + 1, rand.nextInt(width - 2) +1, 1, collisionHandler, this));
-        }
+	for (int i = 0; i < 2; i++) {
+	    objects.add(new Enemy(rand.nextInt(width - 2) + 1, rand.nextInt(width - 2) + 1, collisionHandler, this));
+	    objects.add(new DragonBoss(rand.nextInt(width - 2) + 1, rand.nextInt(width - 2) + 1, collisionHandler, this));
+	    objects.add(new Stone(rand.nextInt(width - 2) + 1, rand.nextInt(width - 2) + 1, 1.5, 1.5, collisionHandler, this));
+	    objects.add(new MovableObject(rand.nextInt(width - 2) + 1, rand.nextInt(width - 2) + 1, collisionHandler, this));
+	    objects.add(new Tree(rand.nextInt(width - 2) + 1, rand.nextInt(width - 2) + 1, 1, collisionHandler, this));
+	}
 
-        int wallWidth = 2;
+	int wallWidth = 2;
 
-        objects.add(new BrickWall(0, height/2 - 1, wallWidth, height, collisionHandler, this));
-        objects.add(new BrickWall(width/2 - 1, height, width, wallWidth, collisionHandler, this));
-        objects.add(new BrickWall(width, height/2 + 1, wallWidth, height, collisionHandler, this));
-        objects.add(new BrickWall(width/2 + 1, 0, width, wallWidth, collisionHandler, this));
+	objects.add(new BrickWall(0, height / 2 - 1, wallWidth, height, collisionHandler, this));
+	objects.add(new BrickWall(width / 2 - 1, height, width, wallWidth, collisionHandler, this));
+	objects.add(new BrickWall(width, height / 2 + 1, wallWidth, height, collisionHandler, this));
+	objects.add(new BrickWall(width / 2 + 1, 0, width, wallWidth, collisionHandler, this));
     }
 
     public List<VisibleObject> getLayers() {
-        return layerList;
+	return layerList;
     }
 
     public void addLayer(VisibleObject object) {
-        layerList.add(object);
+	layerList.add(object);
     }
 
     private void generateBackground() {
-        for (double x = 0.5; x < width*2; x++) {
-            for (double y = 0.5; y < height*2; y++) {
-                backgroundList.add(new Sand(x - width/2, y - height/2, this));
-            }
-        }
+	Random rand = new Random();
 
+	if (rand.nextInt(2) == 0) {
+	    for (double x = 0.5; x < width * 2; x++) {
+		for (double y = 0.5; y < height * 2; y++) {
+			backgroundList.add(new Sand(x - (width / 2), y - height / 2, this));
+		}
+	    }
+
+	} else {
+	    for (double x = 0.5; x < width * 2; x++) {
+		for (double y = 0.5; y < height * 2; y++) {
+			backgroundList.add(new Grass(x - (width / 2), y - height / 2, this));
+		}
+	    }
+	}
     }
 
     public void removeObject(ArenaObject object) {
-        removeObjectsList.remove(object);
+	removeObjectsList.remove(object);
     }
 
     public List<VisibleObject> getBackgroundList() {
-        return backgroundList;
+	return backgroundList;
     }
 
     public List<ArenaObject> getObjects() {
-        return objects;
+	return objects;
     }
 
     public Player getPlayer() {
-        return player;
+	return player;
     }
 
     public boolean isGameOver() {
-        return gameOver;
+	return gameOver;
     }
 
     public void restart() {
-        gameOver = false;
-        backgroundList.clear();
-        layerList.clear();
-        objects.clear();
-        removeObjectsList.clear();
-        collisionHandler.clearAll();
-        generateArena();
+	gameOver = false;
+	backgroundList.clear();
+	layerList.clear();
+	objects.clear();
+	removeObjectsList.clear();
+	collisionHandler.clearAll();
+	generateArena();
     }
 }
