d3211f98234c734e1054fbaaf0ee5b73bb51938b
Rolf Sievert <rolsi701@student.liu.se>
2016-04-28 10:42:48 +0200

    Weakened dragon

--- src/carlorolf/DragonBoss.java
+++ src/carlorolf/DragonBoss.java
@@ -4,7 +4,7 @@ package carlorolf;
 public class DragonBoss extends Enemy {
     public DragonBoss(final double x, final double y, final CollisionHandler collisionHandler, final Arena arena) {
         super(x, y, 2, 2, 1, 100, 2, Images.getImage("dragon_none.png"), collisionHandler, arena);
-        weapon = new Weapon(x, y, 30, 1.5 * width / 6, 0.5, this);
+        weapon = new Weapon(x, y, 30, 1 * width / 6, 0.5, this);
         armor = new Armor(100, this, arena, Images.getImage("dragon_armor.png"));
     }
 
