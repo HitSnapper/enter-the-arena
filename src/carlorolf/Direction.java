2426871fdea81331084d257a37cb1feacbc6d4fe
Rolf Sievert <rolsi701@student.liu.se>
2016-05-01 14:49:20 +0200

    Inspected code.

--- src/carlorolf/Direction.java
+++ src/carlorolf/Direction.java
@@ -3,10 +3,11 @@ package carlorolf;
 /**
  * Direction describes a direction. Has method for getting a vector in the equivalent direction and a toString method.
  */
-enum Direction {
-    NORTH(new Vector(0, -1)), SOUTH(new Vector(0, 1)), EAST(new Vector(1, 0)), WEST(new Vector(-1, 0)),
-    NORTHWEST(new Vector(-1, -1)), NORTHEAST(new Vector(1, -1)), SOUTHWEST(new Vector(-1, 1)), SOUTHEAST(new Vector(1, 1)),
-    NONE(new Vector(0, 0));
+public enum Direction {
+    // These enums doesn't need a javadoc since it's pretty clear what they stand for and it's described in the Directions javadoc.
+    @SuppressWarnings("JavaDoc")NORTH(new Vector(0, -1)), @SuppressWarnings("JavaDoc")SOUTH(new Vector(0, 1)), @SuppressWarnings("JavaDoc")EAST(new Vector(1, 0)), @SuppressWarnings("JavaDoc")WEST(new Vector(-1, 0)),
+    @SuppressWarnings("JavaDoc")NORTHWEST(new Vector(-1, -1)), @SuppressWarnings("JavaDoc")NORTHEAST(new Vector(1, -1)), @SuppressWarnings("JavaDoc")SOUTHWEST(new Vector(-1, 1)), @SuppressWarnings("JavaDoc")SOUTHEAST(new Vector(1, 1)),
+    @SuppressWarnings("JavaDoc")NONE(new Vector(0, 0));
 
     private final Vector vector;
 
